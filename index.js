df0852574ec118ad36b4015797b25166b41e9532
Iuliia El Sammak <juliag875@gmail.com>
2022-05-04 22:26:40 -0400

    commit tree e8626dee4059a640e8df0a4f834f054241ab7622

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i=0; i<word.length/2; i++){
+    let j = word.length - 1 - i
+    if (word[i] !== word[j]){
+      return false
+    }
+  }
+  return true
 }
+
+// function findSock(sortedLaundry) {
+//   let start = 0;
+//   let end = sortedLaundry.length;
+
+//   while (start <= end) {
+//     let mid = Math.floor((start + end) / 2);
+
+//     if (sortedLaundry[mid] === "sock") return "sock";
+
+//     if (sortedLaundry[mid] < "sock") {
+//       start = mid + 1;
+//     } else {
+//       end = mid - 1;
+//     }
+//   }
+// }
 
 /* 
   Add your pseudocode here
+  iterate from the beginning of the string to the middle of the string
+  compare the letter we're iterating over to the corresponding letter at the end of the string
+    if the letters don't match, return false
+
+if we reach the middle, and all the letters match, return true
 */
 
 /*
   Add written explanation of your solution here
+  // iterate from the beginning of the string to the middle of the string
+  // compare the letter we're iterating over to the corresponding letter at the end of the string
+  // if the letters don't match, return false
+  // if we reach the middle, and all the letters match, return true
 */
 
 // You can run `node index.js` to view these console logs
